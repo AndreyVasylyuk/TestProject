6ede3fdfaadf3825069f9c1dde377e4cbb83fe16
Andrey <Andreyvasylyuk@gmail.com>
2020-10-15 23:54:37 +0300

    add files with exceptions

---
+++ src/ua/pp/andreyvasylyuk/exceptions/Person.java
@@ -0,0 +1,33 @@
+package ua.pp.andreyvasylyuk.exceptions;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+    private int id;
+    private String name;
+         
+    public Person(int id, String name) {
+        super();
+        this.id = id;
+        this.name = name;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return id +" : " + name;
+    }
+    
+    
+
+}
---
+++ src/ua/pp/andreyvasylyuk/exceptions/WriteObject.java
@@ -0,0 +1,5 @@
+package ua.pp.andreyvasylyuk.exceptions;
+
+public class WriteObject {
+
+}
